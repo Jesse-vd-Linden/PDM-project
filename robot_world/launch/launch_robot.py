cab0c2311280de0a8b4d041a5cf535b854583865
Jesse <jesse.vd.linden@hotmail.com>
2021-12-20 16:36:04 +0100

    control development effort

--- robot_world/launch/launch_robot.py
+++ robot_world/launch/launch_robot.py
@@ -1,7 +1,7 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 import lxml.etree as etree 
@@ -9,53 +9,36 @@ import yaml
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
-    sdf_file_name = '$(find robot_world)/model/model.sdf'
-
-    print("sdf_file_name : {}".format(sdf_file_name))
-    sdf = os.path.join(sdf_file_name)
+    package_name = 'robot_world'
+    world_file_name = 'world.sdf'
     
-    xml_object = etree.parse('/home/jesse/Documents/pdm/catkin_ws/src/robot_world/model/model.sdf')
-    xml_string = etree.tostring(xml_object).decode()
-    robot_description = DeclareLaunchArgument(
-        "robot_description", default_value=xml_string)
+    world = os.path.join('/home/jesse/Documents/pdm/catkin_ws/src/robot_world/', 'environment',world_file_name)
+    sdf = os.path.join('/home/jesse/Documents/pdm/catkin_ws/src/robot_world/', 'model','servicebot','model.sdf')
+    urdf = os.path.join('/home/jesse/Documents/pdm/catkin_ws/src/robot_world/', 'model','servicebot','model.urdf')
+    print("world_file_name : {}".format(world))
+    print("sdf_file_name : {}".format(sdf))
 
-    yml_string = yaml.load('/home/jesse/Documents/pdm/catkin_ws/src/robot_world/config/config.yml')
-    robot_description = DeclareLaunchArgument(
-        "our_robot", default_value=yml_string)
+    robot_file = etree.parse(urdf)
+    robot_desc = etree.tostring(robot_file).decode()
 
-    # rosparam_controller = DeclareLaunchArgument()
 
     return LaunchDescription([
-        robot_description,
-
-        DeclareLaunchArgument(
-            'robot_world',
-            default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
-        Node(
-            name="urdf_spawner",
-            package="gazebo_ros",
-            executable="spawn_model",
-            respawn="false",
-            output="screen", 
-            arguments="-urdf -model our_robot -param robot_description -y -6"
-        ),
-        Node(
-            name="controller_spawner",
-            package="controller_manager",
-            executable="spawner", 
-            namespace="our_robot",
-            arguments="left_wheel_position_controller right_wheel_position_controller lower_arm_position_controller upper_arm_position_controller joint_state_controller --shutdown-timeout 3"
-        ),
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[{'use_sim_time': use_sim_time}],
-            arguments=[sdf],
-            remappings=[
-                ('/joint_states', '/our_robot/joint_states'),
-            ]
-        )
+        ExecuteProcess(
+            cmd=['gazebo','--verbose',world,'-s','libgazebo_ros_factory.so'],
+            output='screen'),
+        ExecuteProcess(
+            cmd=['ros2','run','gazebo_ros','spawn_entity.py','-entity','our_robot','-x','0','-y','0','-z','0','-R','0','-P','0','-Y','0','-file',sdf],
+            output='screen'),
+        ExecuteProcess(
+            cmd=['ros2','control','load_controller','--set-state','start','joint_state_broadcaster'], #ros2 control load_controller --set-state start joint_state_broadcaster
+            output='screen'),
+        ExecuteProcess(
+            cmd=['ros2','control','load_controller','--set-state','start','joint_trajectory_controller'], #ros2 control load_controller --set-state start joint_trajectory_controller
+            output='screen'),
+        # Node(
+        #     package='robot_state_publisher',
+        #     executable='robot_state_publisher',
+        #     name='robot_state_publisher',
+        #     parameters=[{"robot_description": robot_desc}],
+        #     output='screen'),
     ])
