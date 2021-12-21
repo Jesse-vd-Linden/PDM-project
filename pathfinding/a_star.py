fb4c8d019b0bd8468826548cb0aaab6bed101b54
MrMeepsle <mynameismailtje@gmail.com>
2021-12-21 18:18:35 +0100

    Remove pathfinding nodes.py

--- pathfinding/a_star.py
+++ pathfinding/a_star.py
@@ -8,9 +8,9 @@ class A_star:
         self.freeNodes_ = Map.freeNodes_
         self.obstructedNodes_ = Map.obstructedNodes_
         self.nodeDist_ = Map.nodeDist_
-        self.worldLims_ = {"x": [Map.worldStart_[0], Map.worldXlim_],
-                           "y": [Map.worldStart_[1], Map.worldYlim_]}
+        self.worldLims_ = {"x": [Map.worldStart_[0], Map.worldXlim_], "y": [Map.worldStart_[1], Map.worldYlim_]}
         self.movements_ = self.getMovements()
+        self.maxIterations_ = 1000
 
     def getMovements(self):
         # Add a set of possible movements
@@ -21,6 +21,8 @@ class A_star:
         return movements
 
     def search_path(self, start, end):
+        print('Looking for path...')
+
         # Get suitable start/goal positions
         if start not in self.freeNodes_:
             closest_index = distance.cdist([start], self.freeNodes_).argmin()
@@ -38,14 +40,12 @@ class A_star:
             goal_node = Node(None, end)
         goal_node.g = goal_node.h = goal_node.f = 0
 
-        print('Looking for path...')
-        open = []
+        open = [start_node]  # Start at start node
         closed = []
 
-        # Start at start node
-        open.append(start_node)
-
-        while len(open) > 0:
+        iteration = 0
+        while len(open) > 0 and iteration <= self.maxIterations_:
+            iteration += 1
             current_node = open[0]
             for i in range(len(open)):
                 if open[i].f < current_node.f or open[i].f == current_node.f:
@@ -57,52 +57,61 @@ class A_star:
 
             # Check if goal is found
             if current_node.position == end:
-                print('end found')
-                path = []
-                current = current_node
-                while current is not None:
-                    path.append(current.position)
-                    plt.scatter(current.position[0], current.position[1], color="g")
-                    current = current.parent
-                return path[::-1]
-                # return reverse path and gets out of the loop
-            children = []
-            for new_position in self.movements_:
-                node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
-                if node_position[0] <= np.min(self.worldLims_["x"]) or node_position[0] > np.max(
-                        self.worldLims_["x"]) or node_position[1] <= np.min(self.worldLims_["y"]) or \
-                        node_position[1] > np.max(self.worldLims_["y"]):
-                    continue
+                return self.getPath(current_node)
 
-                if node_position not in self.obstructedNodes_:
-                    new_node = Node(current_node, node_position)
-                    children.append(new_node)
+            # Find adjacent nodes
+            children = self.findAdjacent(current_node)
 
+            # Update heuristics
             for child in children:
                 for closed_child in closed:
                     if child == closed_child:
                         continue
 
-                ##create g,h,f values
                 child.g = current_node.g + 0.5
-                ## heuristic function used
-                child.h = self.GetDistance(child.position, goal_node.position)
+                child.h = self.getDistance(child.position, goal_node.position)  # Heuristic function
+                child.f = child.g + child.h
 
-                child.f = child.g+child.h
-
-                ## check if child is in the open list already
+                # Check if child is in the open list already
                 for open_node in open:
                     if child == open_node and child.g > open_node.g:
                         continue
-
                 open.append(child)
 
-    def GetDistance(self, nodeA, nodeB):
+        print("No path found")
+        return None  # If no path was found
+
+    def getDistance(self, nodeA, nodeB):
         dstX = abs(nodeA[0] - nodeB[0])
         dstY = abs(nodeA[1] - nodeB[1])
         if (dstX > dstY):
             return 14 * dstY + 10 * (dstX - dstY)
         return 14 * dstX + 10 * (dstY - dstX)
+
+    def findAdjacent(self, currentNode):
+        children = []
+        for movement in self.movements_:
+            new_position = (currentNode.position[0] + movement[0], currentNode.position[1] + movement[1])
+            if new_position[0] <= np.min(self.worldLims_["x"]) or \
+                    new_position[0] > np.max(self.worldLims_["x"]) or \
+                    new_position[1] <= np.min(self.worldLims_["y"]) or \
+                    new_position[1] > np.max(self.worldLims_["y"]):
+                continue
+
+            if new_position not in self.obstructedNodes_:
+                new_node = Node(currentNode, new_position)
+                children.append(new_node)
+        return children
+
+    def getPath(self, currentNode):
+        print('end found')
+        path = []
+        current = currentNode
+        while current is not None:
+            path.append(current.position)
+            plt.scatter(current.position[0], current.position[1], color="g")
+            current = current.parent
+        return path[::-1]  # return reverse path and gets out of the loop
 
 
 class Node:
